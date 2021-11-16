a429fb00cccecc5a92e2d8920c4df48a65c8aa50
akilpartheepan191 <81765475+akilpartheepan191@users.noreply.github.com>
2021-11-16 14:02:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,71 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+from PIL import Image
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from PIL import Image, ImageOps
+import numpy as np
+from CaptureImage import *
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+def teachable_machine_classification(img, file):
+    # Disable scientific notation for clarity
+    np.set_printoptions(suppress=True)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+    # Load the model
+    model = keras.models.load_model(file)
 
-    points_per_turn = total_points / num_turns
+    # Create the array of the right shape to feed into the keras model
+    # The 'length' or number of images you can put into the array is
+    # determined by the first position in the shape tuple, in this case 1.
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+    # Replace this with the path to your image
+    image = img
+    # image = Image.open(img_name).convert('RGB')
+    # image = cv2.imread(image)
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    # resize the image to a 224x224 with the same strategy as in TM2:
+    # resizing the image to be at least 224x224 and then cropping from the center
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+    # turn the image into a numpy array
+    image_array = np.asarray(image)
+
+    # display the resized image
+    image.show()
+
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+    # Load the image into the array
+    data[0] = normalized_image_array
+
+    # run the inference
+    prediction = model.predict(data)
+    #print(prediction)
+    return np.argmax(prediction)
+
+
+st.title("AI Police welcomes you back to school!")
+st.header("Mask up or get caught by the police!")
+st.text("Always wear your mask to help contain covid-19.")
+st.button('Click Me, I shall photograph you to check', key='button_photograph',
+          on_click=captureImageFunction)
+
+# file upload and handling logic
+#uploaded_file = st.file_uploader("Upload your JPEG image here,and i will tell you if you are wearing a mask or not", type="jpeg")
+#if uploaded_file is not None:
+    #image = Image.open(uploaded_file).convert('RGB')
+img_name = 'CapturedFrame0.jpg'
+image = Image.open(img_name).convert('RGB')
+st.image(image, caption='Caught your image.', use_column_width=True)
+st.write("")
+st.write("Verifying with my Artificial Intelligence...")
+label = teachable_machine_classification(image, 'keras_model.h5')
+if label == 1:
+   st.write("hmph!Why are'nt you wearing your mask... wear it immediately... or i will call the cops!")
+else:
+        st.write("Good job! You are wearing a mask. Do wear your mask always.")
